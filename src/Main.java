cac71769f5f03b6e68a6341ded57a1f14517f5fd
Grafskii <miti-h@yandex.ru>
2023-05-04 09:45:06 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,16 @@ public class Main {
         int startingScore = 100; // начальные данные
         int price = 100; // цена за каждые 100 рублей
         int amount = 1500;
-        int amount2 = 500;
 
         int bonus = 0;
         if (amount > 1000) {
             bonus = amount / price;
         }
-        int score = startingScore + amount;
-
-        int bonus2 = 0;
-        if (amount2 < 1000) {
-            bonus2 = 0;
-        }
-        int score2 = startingScore + amount2;
+        int score = startingScore + amount + bonus;
 
         System.out.println("Пополнение > 1000");
         System.out.println("Бонус: " + bonus);
         System.out.println("Итоговый счет: " + score);
-        System.out.println("Пополнение < 1000");
-        System.out.println("Бонус: " + bonus2);
-        System.out.println("Итоговый счет: " + score2);
 
     }
 }
